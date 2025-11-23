ff13aa6234db319056888d26d61bcfa4959a6e6c
Ahmed Hodiani <a.hodiani@hotmail.com>
2025-11-23 22:57:45 +0300

    feat: update minimap settings and rendering logic for improved player visibility

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: ataher <ataher@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 00:00:00 by ataher            #+#    #+#             */
-/*   Updated: 2025/11/23 22:11:10 by ataher           ###   ########.fr       */
+/*   Updated: 2025/11/23 22:56:20 by ataher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static void	draw_line(t_game *game, int x0, int y0, int x1, int y1)
 	float	x;
 	float	y;
 	int		i;
+	int		max_x;
+	int		max_y;
 
 	dx = x1 - x0;
 	dy = y1 - y0;
@@ -31,10 +33,14 @@ static void	draw_line(t_game *game, int x0, int y0, int x1, int y1)
 	y_inc = dy / (float)steps;
 	x = x0;
 	y = y0;
+	max_x = MINIMAP_OFFSET_X + MINIMAP_VIEWPORT_TILES * MINIMAP_SCALE;
+	max_y = MINIMAP_OFFSET_Y + MINIMAP_VIEWPORT_TILES * MINIMAP_SCALE;
 	i = 0;
 	while (i <= steps)
 	{
-		my_mlx_pixel_put(&game->img, (int)x, (int)y, MINIMAP_COLOR_RAY);
+		if ((int)x >= MINIMAP_OFFSET_X && (int)x < max_x
+			&& (int)y >= MINIMAP_OFFSET_Y && (int)y < max_y)
+			my_mlx_pixel_put(&game->img, (int)x, (int)y, MINIMAP_COLOR_RAY);
 		x += x_inc;
 		y += y_inc;
 		i++;
@@ -62,11 +68,15 @@ static void	cast_single_ray(t_game *game, double ray_dir_x, double ray_dir_y)
 	int		screen_x;
 	int		screen_y;
 	int		i;
+	int		cam_start_x;
+	int		cam_start_y;
 
 	ray_x = game->player.x;
 	ray_y = game->player.y;
 	step_x = ray_dir_x * 0.05;
 	step_y = ray_dir_y * 0.05;
+	cam_start_x = (int)game->player.x - MINIMAP_VIEWPORT_TILES / 2;
+	cam_start_y = (int)game->player.y - MINIMAP_VIEWPORT_TILES / 2;
 	i = 0;
 	while (i < 200)
 	{
@@ -78,11 +88,13 @@ static void	cast_single_ray(t_game *game, double ray_dir_x, double ray_dir_y)
 			break ;
 		i++;
 	}
-	screen_x = MINIMAP_OFFSET_X + (int)(ray_x * MINIMAP_SCALE);
-	screen_y = MINIMAP_OFFSET_Y + (int)(ray_y * MINIMAP_SCALE);
+	screen_x = MINIMAP_OFFSET_X + (int)((ray_x - cam_start_x) * MINIMAP_SCALE);
+	screen_y = MINIMAP_OFFSET_Y + (int)((ray_y - cam_start_y) * MINIMAP_SCALE);
 	draw_line(game,
-		MINIMAP_OFFSET_X + (int)(game->player.x * MINIMAP_SCALE),
-		MINIMAP_OFFSET_Y + (int)(game->player.y * MINIMAP_SCALE),
+		MINIMAP_OFFSET_X + (MINIMAP_VIEWPORT_TILES / 2) * MINIMAP_SCALE
+			+ (int)((game->player.x - (int)game->player.x) * MINIMAP_SCALE),
+		MINIMAP_OFFSET_Y + (MINIMAP_VIEWPORT_TILES / 2) * MINIMAP_SCALE
+			+ (int)((game->player.y - (int)game->player.y) * MINIMAP_SCALE),
 		screen_x, screen_y);
 }
 
@@ -96,7 +108,7 @@ void	draw_minimap_rays(t_game *game)
 	int		num_rays;
 
 	fov = 60.0 * (3.14159265359 / 180.0);
-	num_rays = 1000;
+	num_rays = 2000;
 	i = 0;
 	while (i < num_rays)
 	{
--- src/rendering.c
+++ src/rendering.c
@@ -6,7 +6,7 @@
 /*   By: ataher <ataher@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 00:00:00 by ataher            #+#    #+#             */
-/*   Updated: 2025/11/23 22:43:07 by ataher           ###   ########.fr       */
+/*   Updated: 2025/11/23 22:56:20 by ataher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,24 +46,40 @@ void	draw_minimap_tile(t_game *game, int x, int y, int color)
 
 void	render_minimap(t_game *game)
 {
-	int		y;
-	int		x;
+	int		map_y;
+	int		map_x;
+	int		screen_y;
+	int		screen_x;
 	int		color;
+	int		cam_start_x;
+	int		cam_start_y;
 
-	y = 0;
-	while (y < game->config.map.height)
+	cam_start_x = (int)game->player.x - MINIMAP_VIEWPORT_TILES / 2;
+	cam_start_y = (int)game->player.y - MINIMAP_VIEWPORT_TILES / 2;
+	screen_y = 0;
+	while (screen_y < MINIMAP_VIEWPORT_TILES)
 	{
-		x = 0;
-		while (x < game->config.map.width)
+		screen_x = 0;
+		while (screen_x < MINIMAP_VIEWPORT_TILES)
 		{
-			if (game->config.map.grid[y][x] == WALL)
-				color = MINIMAP_COLOR_WALL;
-			else if (game->config.map.grid[y][x] == WALKABLE)
-				color = MINIMAP_COLOR_FLOOR;
-			draw_minimap_tile(game, x, y, color);
-			x++;
+			map_x = cam_start_x + screen_x;
+			map_y = cam_start_y + screen_y;
+			if (map_y >= 0 && map_y < game->config.map.height
+				&& map_x >= 0 && map_x < game->config.map.width)
+			{
+				if (game->config.map.grid[map_y][map_x] == WALL)
+					color = MINIMAP_COLOR_WALL;
+				else if (game->config.map.grid[map_y][map_x] == WALKABLE)
+					color = MINIMAP_COLOR_FLOOR;
+				else
+					color = 0x000000;
+			}
+			else
+				color = 0x000000;
+			draw_minimap_tile(game, screen_x, screen_y, color);
+			screen_x++;
 		}
-		y++;
+		screen_y++;
 	}
 }
 
@@ -75,9 +91,15 @@ void	draw_minimap_player(t_game *game)
 	int	screen_y;
 	int	center_x;
 	int	center_y;
+	double	player_offset_x;
+	double	player_offset_y;
 
-	center_x = MINIMAP_OFFSET_X + (int)(game->player.x * MINIMAP_SCALE);
-	center_y = MINIMAP_OFFSET_Y + (int)(game->player.y * MINIMAP_SCALE);
+	player_offset_x = game->player.x - (int)game->player.x;
+	player_offset_y = game->player.y - (int)game->player.y;
+	center_x = MINIMAP_OFFSET_X + (MINIMAP_VIEWPORT_TILES / 2) * MINIMAP_SCALE
+		+ (int)(player_offset_x * MINIMAP_SCALE);
+	center_y = MINIMAP_OFFSET_Y + (MINIMAP_VIEWPORT_TILES / 2) * MINIMAP_SCALE
+		+ (int)(player_offset_y * MINIMAP_SCALE);
 	i = -9;
 	while (i <= 9)
 	{
